210f5ec5145a315ae5d5d146396244a1b5cb5131
thomm.o <olijdthompson@gmail.com>
2021-03-11 16:11:54 +0000

    Update app.py

--- app.py
+++ app.py
@@ -12,17 +12,13 @@ import daily as daily_
 
 app = flask.Flask(__name__)
 POOL = pool.ThreadedConnectionPool(3, 10, os.environ["DATABASE_URL"])
+utils.init_db(POOL)
 
 RUNDOWNS = utils.get_rundown_data()
 
 EMPTY_CARD = [("Primary:", "..."), ("Special:", "..."), ("Utility:", "..."), ("Melee:", "...")]
 PLAYERS = [1, 2, 3, 4]
 VERIFICATION_WEBHOOK = os.environ["VERIFICATION_WEBHOOK"]
-
-
-@app.before_first_request
-def server_setup():
-    utils.init_db(POOL)
 
 
 @app.route("/")
