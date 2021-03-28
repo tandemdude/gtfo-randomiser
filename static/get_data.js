53237a0caa4fbdc04fa1bd6bbffd551cd43dce76
thomm.o <olijdthompson@gmail.com>
2021-03-28 16:58:45 +0100

    revert

--- static/get_data.js
+++ static/get_data.js
@@ -3,13 +3,13 @@ function get_loadout(rundown_id) {
         .then(response => response.json())
         .then(
             data => {
-                for (let key in ["1", "2", "3", "4"]) {
+                for (let key in data) {
                     if (/\d$/.test(key)) {
                         document.getElementById(key).innerHTML = `
-                            <p>Primary: <span class='data'>${data['players'][key]['primary']}</span></p>
-                            <p>Special: <span class='data'>${data['players'][key]['special']}</span></p>
-                            <p>Utility: <span class='data'>${data['players'][key]['utility']}</span></p>
-                            <p>Melee: <span class='data'>${data['players'][key]['melee']}</span></p>
+                            <p>Primary: <span class='data'>${data[key]['primary']}</span></p>
+                            <p>Special: <span class='data'>${data[key]['special']}</span></p>
+                            <p>Utility: <span class='data'>${data[key]['utility']}</span></p>
+                            <p>Melee: <span class='data'>${data[key]['melee']}</span></p>
                         `;
                     } else {
                         document.getElementById("stage").innerHTML = data["stage"]["stage"];
